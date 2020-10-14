799d74cb8608223f9ca7f0787d9619c43f01aef0
bokko-chan <72552210+bokko-chan@users.noreply.github.com>
2020-10-15 08:51:05 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,12 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   has_one_attached :image
-  
-  validates :content, presence: true
+
+  validates :content, presence: true, unless: :was_attached? #was_attached?メソッドは、self.image.attached?という記述によって、画像があればtrue、なければfalseを返す仕組み
+
+  def was_attached?
+    self.image.attached?
+  end
+
 end
+
