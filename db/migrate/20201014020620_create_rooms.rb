a844272836e0072f915f581e0218b31542bad7f8
bokko-chan <72552210+bokko-chan@users.noreply.github.com>
2020-10-14 12:02:29 +0900

    room_usersテーブルの作成

--- db/migrate/20201014020620_create_rooms.rb
+++ db/migrate/20201014020620_create_rooms.rb
@@ -1,6 +1,6 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
-    create_table :rooms do |t|
+    create_table :room_users do |t|
       t.string :name, null: false
       t.timestamps
     end
